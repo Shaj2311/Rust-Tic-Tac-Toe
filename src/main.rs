83dc5bc8a36833b12ef1386fcce69ba5113b936d
Shaj2311 <haideralijafri2007@gmail.com>
2026-01-04 16:26:12 +0500

    Implement board drawing and cell value checking

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+#[derive(PartialEq, PartialOrd)]
+pub enum CellValue
+{
+    BLANK,
+    PLAYER1,
+    PLAYER2
+}
+use CellValue::*;
+
 fn main() {
-    println!("Hello, world!");
+    //initialize board
+    let mut board: [[CellValue; 3]; 3] = [
+        [BLANK,BLANK,BLANK],
+        [BLANK,BLANK,BLANK],
+        [BLANK,BLANK,BLANK]
+    ];
+    //player 1 starts
+    let mut player_1_turn = true;
+
+    draw_board(&board);
+}
+
+//print board
+fn draw_board(board: &[[CellValue;3];3]) {
+    for _i in 0..7
+    {
+        print!("-");
+    }
+    println!("");
+
+    for i in 0..3
+    {
+
+        print!("|");
+
+        for j in 0..3
+        {
+            print!("{}|",
+                 if board[i][j] == PLAYER1 {'O'}
+                else if board[i][j] == PLAYER2 {'X'}
+                else {' '}
+            );
+        }
+
+        println!("");
+        for _i in 0..7
+        {
+            print!("-");
+        }
+        println!("");
+
+    }
 }
